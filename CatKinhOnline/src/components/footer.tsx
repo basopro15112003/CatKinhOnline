e34d886a773faa5027e13bef2b5bee098f37eef6
basopro1511 <nguyenquochoang2003ct@gmail.com>
2025-05-31 20:15:33 +0700

    Done Login Register UI from

---
+++ CatKinhOnline/src/components/footer.tsx
@@ -0,0 +1,89 @@
+import {
+  Facebook,
+  Instagram,
+  Mail,
+  MapPin,
+  Phone,
+  Twitter,
+} from "lucide-react";
+
+export function Footer() {
+  return (
+    <>
+      <footer className="bg-green-800 text-white py-8">
+        <div className="max-w-7xl rounded-2xl mx-auto grid grid-cols-1 md:grid-cols-4 gap-6">
+          <div>
+            <h3 className="text-2xl font-semibold mb-4">
+              Tiệm kính Quốc Hoàng
+            </h3>
+            <p className="flex items-center gap-2 text-sm mb-2">
+              <MapPin size={20} />
+              227 Phong Điền, TP Cần Thơ, Việt Nam
+            </p>
+            <p className="flex items-center gap-2 text-sm mb-2">
+              <Mail size={20} /> quochoangnguyen2003ct@gmail.com
+            </p>
+            <p className="flex items-center gap-2 text-sm mb-2">
+              <Phone size={20} /> 0901 234 567
+            </p>
+          </div>
+
+          <div>
+            <h4 className="text-2xl font-semibold mb-4 ">Liên kết</h4>
+            <ul className="space-y-1 text-sm">
+              <li className="mb-2">
+                <a href="#" className="hover:underline">
+                  Giới thiệu
+                </a>
+              </li>
+              <li className="mb-2">
+                <a href="#" className="hover:underline">
+                  Liên hệ
+                </a>
+              </li>
+              <li className="mb-2">
+                <a href="#" className="hover:underline">
+                  Chính sách bảo mật
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h4 className="text-2xl font-semibold mb-4 ">Tài khoản</h4>
+            <ul className="space-y-1 text-base">
+              <li className="mb-2">
+                <a href="#" className="hover:underline">
+                  Đăng nhập
+                </a>
+              </li>
+              <li className="mb-2">
+                <a href="#" className="hover:underline">
+                  Đặt Đơn
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="">
+            <h4 className="text-2xl font-semibold mb-4">
+              Kết nối với chúng tôi
+            </h4>
+            <div className="flex space-x-4">
+              <a href="#" aria-label="Facebook">
+                <Facebook size={20} />
+              </a>
+              <a href="#" aria-label="Instagram">
+                <Instagram size={20} />
+              </a>
+              <a href="#" aria-label="Twitter">
+                <Twitter size={20} />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="text-center text-xs text-white mt-7">
+          © 2025 QuocHoang. All rights reserved.
+        </div>
+      </footer>
+    </>
+  );
+}
---
+++ CatKinhOnline/src/pages/login.tsx
@@ -0,0 +1,55 @@
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+interface LoginProps {
+  setShowLogin: (value: boolean) => void;
+  setShowRegister: (value: boolean) => void;
+}
+export function Login({setShowLogin, setShowRegister}: LoginProps) {
+
+  return (
+    <>
+    <div className="fixed inset-0 bg-black/50 backdrop-blur-xs flex items-center justify-center z-50">
+          <Card className="w-full max-w-lg">
+            <form>
+              <CardHeader className="p-6">
+                <CardTitle>Đăng nhập vào tài khoản của bạn</CardTitle>
+                <CardDescription>Nhập email của bạn vào ô ở bên dưới để đăng nhập vào tài khoản </CardDescription>
+              </CardHeader>
+              <CardContent className="px-6 mb-6">
+                <div className="grid gap-4">
+                  <div className="grid gap-2">
+                    <Label htmlFor="email">Tài khoản</Label>
+                    <Input id="email" type="email" placeholder="quochoangnguyen2003ct@example.com" required />
+                  </div>
+                  <div className="grid gap-2 mb-2">
+                    <div className="flex items-center">
+                      <Label htmlFor="password">Mật khẩu</Label>
+                      <a href="#" className="ml-auto text-sm underline hover:underline-offset-2">Quên mật khẩu?</a>
+                    </div>
+                    <Input id="password" type="password"  />
+                  </div>
+                </div>
+                 <CardDescription>Nếu như bạn chưa có tài khoản hãy "<span className="text-blue-800 hover:underline hover:text-blue-900 cursor-pointer" onClick={() =>{setShowRegister(true); setShowLogin(false)}}> đăng ký </span> " tài khoản mới</CardDescription>
+              </CardContent>
+              <CardFooter className="flex-col gap-2 px-6 pb-6">
+                <Button type="submit" className="w-full">Đăng nhập</Button>
+                <Button variant="outline" className="w-full">Đăng nhập với Google</Button>
+                <Button variant="ghost" className="w-full text-sm text-gray-500" onClick={() => setShowLogin(false)}>Huỷ</Button>
+              </CardFooter>
+            </form>
+          </Card>
+        </div>   
+
+         </>
+  )
+}
